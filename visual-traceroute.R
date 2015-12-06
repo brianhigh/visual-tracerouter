eecc68342da6d6247e6aed2ee333ea4eaa454f3c
Brian High <bhigh@live.com>
2015-12-05 17:55:58 -0800

    Fixed bug where images not saving on Linux systems when run from Rscript.

--- visual-traceroute.R
+++ visual-traceroute.R
@@ -86,7 +86,7 @@ create_folders_and_filenames <- function(file.addr, data.dir, images.dir) {
         route.csv.file = file.path(data.dir, file.addr, "route.csv"),
         ipinfo.csv.file = file.path(data.dir, file.addr, "ipinfo.csv"),
         map.png.file = file.path(images.dir, file.addr, "map.png"),
-        ggmap.png.file = paste0(images.dir, file.addr, "ggmap.png"),
+        ggmap.png.file = file.path(images.dir, file.addr, "ggmap.png"),
         stringsAsFactors=FALSE
     )
     
@@ -194,14 +194,13 @@ get_bbox <- function(ipinfo) {
 
 plot_ggmap <- function(ipinfo) {
     # Plot using the ggmap package.
+    x11()
     library(ggmap)
     p <- qmplot(longitude, latitude, data = ipinfo, 
                 maptype = "toner-lite", color = I("red"), 
                 geom = "segment", xend=next_longitude, yend=next_latitude)
     print(p)
     
-    # Bug: Does not save properly when the R script is run from Rscript.
-    # However, your plot will be saved in Rplots.pdf within the current folder.
     if (save.plot == TRUE) {
         dev.copy(png, files$ggmap.png.file)
         dev.off()
@@ -210,6 +209,7 @@ plot_ggmap <- function(ipinfo) {
 
 plot_maps <- function(ipinfo, bbox) {
     # Plot using the maps package.
+    x11()
     library(maps)
     map("world", xlim=c(bbox$minlon,bbox$maxlon), 
         ylim=c(bbox$minlat,bbox$maxlat), 
@@ -217,8 +217,6 @@ plot_maps <- function(ipinfo, bbox) {
     points(x = ipinfo$longitude, y = ipinfo$latitude, col = "red")
     lines(x = ipinfo$longitude, y = ipinfo$latitude, col = "blue")
     
-    # Bug: Does not save properly when the R script is run from Rscript.
-    # However, your plot will be saved in Rplots.pdf within the current folder.
     if (save.plot == TRUE) {
         dev.copy(png, files$map.png.file)
         dev.off()
